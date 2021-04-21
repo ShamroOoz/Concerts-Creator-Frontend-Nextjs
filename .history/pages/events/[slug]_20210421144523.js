e6830e4e2534b99888ba3badd977acab3b6c234d
shamroz73 <shamrozwarraich@gmail.com>
2021-04-21 14:51:49 +0200

    setting layout

---
+++ .history/pages/events/[slug]_20210421144523.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+
+export default function EventPage() {
+  return (
+    <div>
+      <Layout>
+        <h1>My Event</h1>
+      </Layout>
+    </div>
+  );
+}
