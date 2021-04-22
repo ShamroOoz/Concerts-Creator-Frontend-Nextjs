0a1f30bd91bd5ea46ede6b1a276dfffcc6779c8c
shamroz73 <shamrozwarraich@gmail.com>
2021-04-23 01:33:01 +0200

    logout register

--- .history/context/AuthContext_20210423012832.js
+++ .history/context/AuthContext_20210423012832.js
@@ -56,16 +56,9 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
-  // Logout user
-  const logout = async () => {
-    const res = await fetch(`${NEXT_URL}/api/logout`, {
-      method: "POST",
-    });
-
-    if (res.ok) {
-      setUser(null);
-      router.push("/");
-    }
+  //logout
+  const logout = async (user) => {
+    console.log(user);
   };
 
   // Check if user is logged in
